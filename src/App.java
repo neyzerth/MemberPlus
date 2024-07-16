27f2dc28ae59c4d3e991e03eb62e2ab3dc00300d
Neyzer <neyzertoledo@gmail.com>
2024-07-15 19:12:02 -0700

    si

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ola papus");
+        System.out.println("ola papus");
+        System.out.println("ola papus");
+        System.out.println("ola papus");
+        System.out.println("ola papus");
+        System.out.println("ola papus");
+        System.out.println("ola papus");
+        System.out.println("ola papus");
+        System.out.println("ola papus");
     }
 }
