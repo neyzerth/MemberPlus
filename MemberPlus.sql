18b0f9f36ac30956bf0c280b2c84092793bf065c
Neyzer <neyzertoledo@gmail.com>
2024-07-18 14:18:14 -0700

    Se creo la base de datos con archivo SQL

--- MemberPlus.sql
+++ MemberPlus.sql
@@ -1,32 +1,121 @@
+-- Active: 1719648459430@@localhost@3306@memberplus
 CREATE DATABASE member_plus;
-use member plus;
-CREATE TABLE persona(
-    id_empleado INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    telefono VARCHAR(12) NOT NULL,
-    correo VARCHAR(40),
+
+use member_plus;
+
+CREATE TABLE persona (
+    idPersona INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre varchar(15) NOT NULL,
+    apellidoMa varchar(15) NOT NULL,
+    apellidoPa varchar(15),
     fecNac DATE NOT NULL,
-    nom_pila varchar(15) NOT NULL,
-    nom_apellidoMa varchar(15) NOT NULL,
-    nom_apellidoPa varchar(15)
+    colonia VARCHAR(30),
+    calle VARCHAR(30),
+    numExt INT, 
+    numInt INT,
+    cp varchar(10),
+    telefono VARCHAR(12) NOT NULL,
+    correo VARCHAR(40)
 );
 
-CREATE TABLE tarjeta(
+Create table cliente (
+    idCliente INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    persona INT NOT NULL,
+    Foreign Key (persona) REFERENCES persona (idPersona)
+)
+-- SQLBook: Code
+CREATE Table rol (
+    idRol INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL
+);
+
+-- Se tiene que primero crear la tabla rol para hacer la fk
+CREATE Table usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombreUsuario VARCHAR(20) not null,
+    contrasena VARCHAR(20) not NULL,
+    rfc VARCHAR(15),
+    persona INT NOT NULL,
+    rol int NOT null,
+    FOREIGN KEY (rol) REFERENCES rol (idRol),
+    Foreign Key (persona) REFERENCES persona (idPersona)
+);
+
+CREATE TABLE tarjeta (
     numTarjeta BIGINT(16) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    fec_Exp DATE NOT NULL,
-    fec_Ven DATE NOT NULL,
-    activo BOOLEAN NOT NULL,
-    saldo DOUBLE NOT NULL,
-    puntos INT 10 NOT NULL
+    fecExp DATE NOT NULL,
+    fecVen DATE NOT NULL,
+    activo BIT NOT NULL,
+    saldo FLOAT NOT NULL,
+    puntos INT  NOT NULL,
+    cliente INT NOT NULL,
+    FOREIGN KEY (cliente) REFERENCES cliente (idcliente)
+)
+
+
+Create table compra (
+    idCompra INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    fecha DATE NOT NULL,
+    porcPunto int,
+    descuento int,
+    tarjeta BIGINT(16) not null,
+    total FLOAT NOT NULL,
+    FOREIGN KEY (tarjeta) REFERENCES tarjeta(numTarjeta) 
+)
+
+
+CREATE TABLE nivel (
+    idNivel INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre varchar(20) NOT NULL,
+    anualidad int NOT NULL, 
+    costoApertura int NOT NULL
+)
+
+CREATE TABLE beneficios (
+    idBeneficio INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(25) NOT NULL,
+    fecInicio Date NOT NULL,
+    fecVen DATE NOT NULL,
+    porcentajePuntos int,
+    porcentajeCashBack int
+)
+
+CREATE table tipo_movimiento(
+    idTipoMovimiento INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    descripcion VARCHAR(100)
+)
+
+
+--No corre
+create table movimiento(
+    idMovimiento INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    fecMovimiento DATE NOT NULL,
+    estado BIT,
+    comentario VARCHAR(100),
+    usuario int not null,
+    tarjeta BIGINT(16) not null,
+    tipoMovimiento int not null,
+    FOREIGN KEY (usuario) REFERENCES usuario(idUsuario),
+    FOREIGN KEY (tarjeta) REFERENCES tarjeta(numTarjeta),
+    FOREIGN KEY (tipoMovimiento) REFERENCES tipo_movimiento(idTipoMovimiento)
 );
 
-CREATE TABLE nivel(
-    id_nivel INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nombre varchar(20) NOT NULL,
-    anualidad DOUBLE NOT NULL,
-    costo DOUBLE NOT NULL,
+--Checar como se hace estas tablas
+CREATE TABLE nivel_beneficio (
+    nivel int not null,
+    beneficio int not null,
+    PRIMARY KEY(nivel,beneficio),
+    FOREIGN KEY (Nivel) REFERENCES nivel(idNivel),
+    FOREIGN KEY (Beneficio) REFERENCES beneficios(idBeneficio)
 );
-CREATE TABLE beneficios(
-    id_beneficio INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nombre VARCHAR(25) NOT NULL,
-    fecVen DATE NOT NULL,
+
+
+Create Table compra_beneficio (
+    compra int not null,
+    beneficio int not null,
+    PRIMARY KEY(compra,beneficio),
+    FOREIGN KEY (compra) REFERENCES compra(idCompra),
+    FOREIGN KEY (beneficio) REFERENCES beneficios(idBeneficio)
 );
