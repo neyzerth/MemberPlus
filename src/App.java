d6d8749c48431b931067ea794dfeaa6630187831
Axel <yerlan_2004@outlook.com>
2024-07-18 09:43:17 -0700

    le puse a

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import Persistencia.Conexion;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ola papus");
+        System.out.println("ola papusa");
         Conexion conexion = new Conexion();
         conexion.conectar();
     }
