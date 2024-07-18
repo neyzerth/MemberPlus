ad65edb232238a2d0e053ae7a303a3126aea754f
Neyzer <neyzertoledo@gmail.com>
2024-07-18 09:31:35 -0700

    Prueba conexion

--- src/App.java
+++ src/App.java
@@ -1,13 +1,9 @@
+import Persistencia.Conexion;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ola papus");
-        System.out.println("ola papus");
-        System.out.println("ola papus");
-        System.out.println("ola papus");
-        System.out.println("ola papus");
-        System.out.println("ola papus");
-        System.out.println("ola papus");
-        System.out.println("ola papus");
-        System.out.println("ola papus");
+        Conexion conexion = new Conexion();
+        conexion.conectar();
     }
 }
