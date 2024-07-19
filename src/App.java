e95e33a1ca93de51ea54e187003592c5421c6247
neyzer <neyzertoledo@gmail.com>
2024-07-19 13:22:34 -0700

    Metodos para conectar a localhost y a vlan60

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import Persistencia.Conexion;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("ola papusa");
-        Conexion conexion = new Conexion();
-        conexion.conectar();
-    }
-}
--- src/Persistencia/Conexion.java
+++ src/Persistencia/Conexion.java
@@ -4,11 +4,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    String db="almacen2d";
-    String host="192.168.60.20";
-    String url = "jdbc:mysql://"+ host +"/" + db;
-    String user = "admin";    //Valores por defecto
-    String password = "12345678";   //Valores por defecto
+    String db = "member_plus";
+    String host, url, user, password;
     
     //Conexion
     public Connection conectar(){
@@ -18,11 +15,22 @@ public class Conexion {
             System.out.println("Conexion exitosa a la base de datos");
             connection.close();
         } catch(SQLException e){
-            System.out.println("Fallo de conexion");
-
-            e.printStackTrace();
+            System.out.println("Fallo de conexion: " + e.getMessage());
         }
         return connection;
+    }
+
+    public void setVlan(){
+        host = "192.168.60.20";
+        url = "jdbc:mysql://"+ host +"/" + db;
+        user = "admin";
+        password = "12345678";
+    }
+    public void setLocal(){
+        host = "localhost";
+        url = "jdbc:mysql://"+ host +"/" + db;
+        user = "root";
+        password = "";
     }
     
 }
