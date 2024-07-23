df9758621f1df5f553329a7c31671bbd0cb19114
Neyzer <neyzertoledo@gmail.com>
2024-07-22 22:04:40 -0700

    Le quite los import de sql

--- src/Presentacion/Menu.java
+++ src/Presentacion/Menu.java
@@ -1,12 +1,6 @@
 package Presentacion;
 
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Menu {
 
     public static void main(String[] args) {
--- src/Presentacion/MenuPrincipal.java
+++ src/Presentacion/MenuPrincipal.java
@@ -1,11 +1,6 @@
 package Presentacion;
 
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class MenuPrincipal {
     public static void main(String[] args) {
