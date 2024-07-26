e15f49ca8cdad0345e931ec61a1c0d06f5289455
Neyzer <neyzertoledo@gmail.com>
2024-07-25 19:40:29 -0700

    Abstraer funciones

--- src/Persistencia/Tablas/Query.java
+++ src/Persistencia/Tablas/Query.java
@@ -1,13 +1,15 @@
 package Persistencia.Tablas;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import java.util.Date;
 import Persistencia.Conexion;
 
 public class Query {
+
     private String tabla;
     private String [] nomColumnas;
 
@@ -64,11 +66,216 @@ public class Query {
         return registros;
     }
 
-    public String selectUno(){
-        String query = select() + "WHERE " + nomColumnas[0] + "= ?";
+    public String selectUno(String columna){
+        String query = select() + " WHERE " + columna + " = ?";
         return query;
-    };
+    }
 
+    public Object[] ejecutarSelectUno(String columna, Object valor) {
+        Object[] registro = null;
+        Conexion conexion = new Conexion();
+        Connection conn = conexion.conectar();
+
+        if (conn != null) {
+            try {
+                PreparedStatement pstmt = conn.prepareStatement(selectUno(columna));
+                pstmt.setObject(1, valor);
+
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    registro = new Object[getCantColumnas()];
+                    for (int i = 0; i < getCantColumnas(); i++) {
+                        registro[i] = rs.getObject(getNomColumna(i));
+                    }
+                }
+
+                rs.close();
+                pstmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return registro;
+    }
+
+    public Object[] ejecutarSelectPorID(int valor){
+        return ejecutarSelectUno(getNomColumna(1), valor);
+    }
+
+    public boolean existeRegistro(String columna, Object valor) {
+        boolean existe = false;
+        Conexion conexion = new Conexion();
+        Connection conn = conexion.conectar();
+
+        if (conn != null) {
+            try {
+                String query = "SELECT EXISTS (SELECT 1 FROM tarjeta WHERE" + columna + "= ?)";
+                PreparedStatement pstmt = conn.prepareStatement(query);
+                pstmt.setObject(1, valor);
+
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    existe = rs.getBoolean(1);
+                }
+
+                rs.close();
+                pstmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return existe;
+    }
+
+
+    public String insert(Object[] valores) {
+        String query = "INSERT INTO " + tabla + " (";
+        
+        // Construir la lista de columnas
+        for (int i = 0; i < nomColumnas.length; i++) {
+            query += nomColumnas[i];
+            if (i != nomColumnas.length - 1) {
+                query += ", ";
+            } else {
+                query += ") VALUES (";
+            }
+        }
+        
+        // Construir la lista de valores
+        for (int i = 0; i < valores.length; i++) {
+            if (valores[i] instanceof String || valores[i] instanceof Date) {
+                query += "'" + valores[i] + "'";
+            } else {
+                query += valores[i];
+            }
+            
+            if (i != valores.length - 1) {
+                query += ", ";
+            }
+        }
+        
+        query += ")";
+        
+        return query;
+    }
+
+    public boolean ejecutarInsert(String query) {
+        Conexion conexion = new Conexion();
+        Connection conn = conexion.conectar();
+        boolean resultado = false;
+
+        if (conn != null) {
+            try {
+                Statement stmt = conn.createStatement();
+                int filasInsertadas = stmt.executeUpdate(query);
+
+                if (filasInsertadas > 0) {
+                    resultado = true;
+                }
+
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return resultado;
+    }
+
+    public String update(Object[] valores, int id) {
+        String query = "UPDATE " + tabla + " SET ";
+    
+        // Construir la lista de columnas y valores a actualizar
+        for (int i = 0; i < nomColumnas.length; i++) {
+            query += nomColumnas[i] + " = ";
+            
+            if (valores[i] instanceof String || valores[i] instanceof Date) {
+                query += "'" + valores[i] + "'";
+            } else {
+                query += valores[i];
+            }
+    
+            if (i != nomColumnas.length - 1) {
+                query += ", ";
+            } else {
+                query += " ";
+            }
+        }
+    
+        query += "WHERE " + nomColumnas[0] + " = " + id;
+    
+        return query;
+    }
+    
+
+    public boolean ejecutarUpdate(String query) {
+        Conexion conexion = new Conexion();
+        Connection conn = conexion.conectar();
+        boolean resultado = false;
+
+        if (conn != null) {
+            try {
+                Statement stmt = conn.createStatement();
+                int filasActualizadas = stmt.executeUpdate(query);
+
+                if (filasActualizadas > 0) {
+                    resultado = true;
+                }
+
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return resultado;
+    }
+
+    public String delete(int id) {
+        String query = "DELETE FROM " + tabla + " WHERE " + nomColumnas[0] + " = " + id;
+        return query;
+    }
+
+    public boolean ejecutarDelete(String query) {
+        Conexion conexion = new Conexion();
+        Connection conn = conexion.conectar();
+        boolean resultado = false;
+
+        if (conn != null) {
+            try {
+                Statement stmt = conn.createStatement();
+                int filasEliminadas = stmt.executeUpdate(query);
+
+                if (filasEliminadas > 0) {
+                    resultado = true;
+                }
+
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return resultado;
+    }
+
+     // Método para formatear un registro de la tabla como cadena para cuando busquemos un registro salga asi 
+     //idTarjeta: 17, numTarjeta: 2, fecExp: 2020-02-02, fecVen: 2020-02-02, activo: true, saldo: 1.0, puntos: 1, cliente: 1, nivel: 1
+    public String formatearRegistro(Object[] registro) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < registro.length; i++) {
+            sb.append(nomColumnas[i]).append(": ").append(registro[i]);
+            if (i < registro.length - 1) {
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
 
     public String getTabla() {
         return this.tabla;
@@ -94,9 +301,8 @@ public class Query {
         this.nomColumnas[i] = nomColumna;
     }
 
+
     public int getCantColumnas(){
         return nomColumnas.length;
     }
-
-    
 }
--- src/Persistencia/Tablas/TarjetaEnt.java
+++ src/Persistencia/Tablas/TarjetaEnt.java
@@ -1,134 +1,57 @@
 package Persistencia.Tablas;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import Persistencia.Conexion;
+import java.sql.Date;
 
 
-//Falto trar el nivel jajaja
-public class TarjetaEnt {
+public class TarjetaEnt extends Query {
 
-    public Object[][] obtenerTodasLasTarjetas() {
-        Object[][] tarjetaArray = null;
-        Conexion conexion = new Conexion();
-        Connection conn = conexion.conectar();
-
-        if (conn != null) {
-            try {
-                String query = "SELECT idTarjeta, numTarjeta, fecExp, fecVen, activo, saldo, puntos, cliente FROM tarjeta";
-                
-                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                ResultSet rs = stmt.executeQuery(query);
-
-                rs.last();
-                int rowCount = rs.getRow();
-                rs.beforeFirst();
-
-                tarjetaArray = new Object[rowCount][8];
-
-                int rowIndex = 0;
-                while (rs.next()) {
-                    tarjetaArray[rowIndex][0] = rs.getInt("idTarjeta");
-                    tarjetaArray[rowIndex][1] = rs.getString("numTarjeta");
-                    tarjetaArray[rowIndex][2] = rs.getDate("fecExp");
-                    tarjetaArray[rowIndex][3] = rs.getDate("fecVen");
-                    tarjetaArray[rowIndex][4] = rs.getBoolean("activo");
-                    tarjetaArray[rowIndex][5] = rs.getFloat("saldo");
-                    tarjetaArray[rowIndex][6] = rs.getInt("puntos");
-                    tarjetaArray[rowIndex][7] = rs.getInt("cliente");
-                    rowIndex++;
-                }
-
-                rs.close();
-                stmt.close();
-                conn.close();
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return tarjetaArray;
+    public TarjetaEnt() {
+        super("tarjeta",
+                "idTarjeta",
+                "numTarjeta",
+                "fecExp",
+                "fecVen",
+                "activo",
+                "saldo",
+                "puntos",
+                "cliente",
+                "nivel");
+    }
+    
+    // Funciones para operar tarjetas
+    public Object [] obtenerTarjetaPorId(int idTarjeta) {
+        return ejecutarSelectPorID(idTarjeta);
     }
 
- //Funcion que se puede utlizar en otra capa 
-    public void imprimirTarjeta(Object[][] tarjetaArray) {
-        for (Object[] row : tarjetaArray) {
-            System.out.println("ID: " + row[0] + ", Número: " + row[1] + ", Fecha Exp: " + row[2] +
-                               ", Fecha Ven: " + row[3] + ", Activo: " + row[4] + ", Saldo: " + row[5] +
-                               ", Puntos: " + row[6] + ", Cliente: " + row[7]);
-        }
+    // Método para interactuar con el usuario y mostrar la tarjeta por número
+    public Object [] obtenerTarjetaPorNum(String numTarjeta) {
+        return ejecutarSelectUno(getNomColumna(1),numTarjeta);
     }
 
-    public boolean actualizarTarjeta(int idTarjeta, String numTarjeta, java.sql.Date fecExp, java.sql.Date fecVen, boolean activo, float saldo, int puntos, int cliente) {
-        Conexion conexion = new Conexion();
-        Connection conn = conexion.conectar();
-        boolean actualizado = false;
-
-        if (conn != null) {
-            try {
-                String updateQuery = "UPDATE tarjeta SET numTarjeta = ?, fecExp = ?, fecVen = ?, activo = ?, saldo = ?, puntos = ?, cliente = ? WHERE idTarjeta = ?";
-                PreparedStatement pstmt = conn.prepareStatement(updateQuery);
-                pstmt.setString(1, numTarjeta);
-                pstmt.setDate(2, fecExp);
-                pstmt.setDate(3, fecVen);
-                pstmt.setBoolean(4, activo);
-                pstmt.setFloat(5, saldo);
-                pstmt.setInt(6, puntos);
-                pstmt.setInt(7, cliente);
-                pstmt.setInt(8, idTarjeta);
-
-                int rowsAffected = pstmt.executeUpdate();
-                actualizado = (rowsAffected > 0);
-
-                pstmt.close();
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return actualizado;
+    // Actualizar una tarjeta
+    public boolean actualizarTarjetaDB(int idTarjeta, String numTarjeta, Date fecExp, Date fecVen,
+            boolean activo, float saldo, int puntos, int cliente, int nivel) {
+        Object[] valores = { idTarjeta, numTarjeta, fecExp, fecVen, activo, saldo, puntos, cliente, nivel };
+        String query = update(valores, idTarjeta);
+        return ejecutarUpdate(query);
     }
 
-    //Funcion que deberia de ir en otra capa 
-    //ponerle un nombre mas adeucado
-    public  void realizarOperaciones() {
-        try {
-            TarjetaEnt tarjetaEnt = new TarjetaEnt();
-
-        // Obtener y imprimir todas las tarjetas se imprime antes para ver los datos
-        Object[][] tarjetaArray = tarjetaEnt.obtenerTodasLasTarjetas();
-        tarjetaEnt.imprimirTarjeta(tarjetaArray);
-
-        // Datos de ejemplo para actualizar 
-        //se tiene que hacer con entrada de teclado
-        int idTarjeta = 1;
-        String numTarjeta = "1234567890123456";
-        java.sql.Date fecExp = java.sql.Date.valueOf("2023-01-01");
-        java.sql.Date fecVen = java.sql.Date.valueOf("2026-01-01");
-        boolean activo = true;
-        float saldo = 179;
-        int puntos = 109;
-        int cliente = 1;
-
-        boolean resultado = tarjetaEnt.actualizarTarjeta(idTarjeta, numTarjeta, fecExp, fecVen, activo, saldo, puntos, cliente);
-
-        if (resultado) {
-            System.out.println("La tarjeta se actualizó correctamente.");
-        } else {
-            System.out.println("Hubo un error al actualizar la tarjeta.");
-        }
-
-        // Obtener y imprimir todas las tarjetas nuevamente después de la actualización
-        tarjetaArray = tarjetaEnt.obtenerTodasLasTarjetas();
-        tarjetaEnt.imprimirTarjeta(tarjetaArray);
-            
-        } catch (Exception e) {
-            e.printStackTrace();    
-        }
-        
+    // Insertar una tarjeta
+    public boolean insertarTarjetaDB(int idTarjeta, String numTarjeta, Date fecExp, Date fecVen, boolean activo,
+            float saldo, int puntos, int cliente, int nivel) {
+        Object[] valores = { idTarjeta, numTarjeta, fecExp, fecVen, activo, saldo, puntos, cliente, nivel };
+        String query = insert(valores);
+        return ejecutarInsert(query);
     }
+    // Verificar si el numTarjeta ya existe en la base de datos
+    public boolean existeNumTarjeta(String numTarjeta) {
+        return existeRegistro(getNomColumna(1), numTarjeta);
+    }
+
+    // Eliminar una tarjeta
+    public boolean eliminarTarjetaDB(int idTarjeta) {
+        String query = delete(idTarjeta);
+        return ejecutarDelete(query);
+    }
+
 }
