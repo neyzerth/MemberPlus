f11a60da9e7d00569bd23a3388d588a4511a94c0
Axel <yerlan_2004@outlook.com>
2024-08-04 22:55:59 -0700

    Cambios al banco

--- src/BD/Registros.sql
+++ src/BD/Registros.sql
@@ -2,26 +2,27 @@
 -- Active: 1722578640281@@127.0.0.1@3306@member_plus
 --CREAR REGISTROS EN LA BASE DE DATOS
 INSERT INTO persona (idPersona, nombre, apellidoPa, apellidoMa, fecNac, colonia, calle, numExt, numInt, cp, telefono, correo) VALUES 
-(1, 'Jane', 'Smith', 'Doe', '1999-11-10', 'Centro', 'Calle Primera', 123, NULL, '22100', '6641112223', 'jane.doe@example.com'),
-(2, 'Mike', 'Brown', 'Jones', '1980-02-15', 'Chapultepec', 'Avenida Revolucion', 456, NULL, '22200', '6642223334', 'mike.jones@emailprovider.com'),
-(3, 'Lucy', 'Williams', 'Liu', '1990-03-20', 'La Joya', 'Boulevard Agua Caliente', 789, NULL, '22300', '6643334445', 'lucy.liu@domain.org'),
-(4, 'Mark', 'Martinez', 'Taylor', '2000-04-20', 'El Dorado', 'Callejon del Sol', 101, NULL, '22400', '6644445556', 'salesrep@companyname.com'),
-(5, 'Sophia', 'Davis', 'Wilson', '2001-05-05', 'Los Alamos', 'Avenida de la Paz', 202, NULL, '22500', '6645556667', 'gamer101@emailservice.net'),
-(6, 'Liam', 'Garcia', 'Johnson', '2003-06-12', 'San Angel', 'Calle Lazaro Cardenas', 303, NULL, '22600', '6646667778', 'petlover@emailhosting.biz'),
-(7, 'Olivia', 'Rodriguez', 'Martinez', '2005-07-18', 'La Cumbre', 'Boulevard Diaz Ordaz', 404, NULL, '22700', '6647778889', 'consultant123@emailprovider.co.uk'),
-(8, 'Emma', 'Hernandez', 'Brown', '1979-08-22', 'Bella Vista', 'Callejon de los Rios', 505, NULL, '22800', '6648889990', 'anna.brown@freemail.com'),
-(9, 'Noah', 'Lopez', 'Clark', '1988-09-07', 'Las Palmas', 'Avenida Constitucion', 606, NULL, '22900', '6649990001', 'ducklover@gmail.com'),
-(10, 'Ava', 'Gonzalez', 'Harris', '1982-10-09', 'Villa del Prado', 'Calle Benito Juarez', 707, NULL, '23000', '6640001112', 'mutunui.joahna@hotmail.com'),
-(11, 'Joao', 'Gomes', 'Silva', '2000-01-10', 'Parnell', 'Queen Street', 701, NULL, '19091', '6631246722', 'emily.johnson@example.com'),
-(12, 'Francisco', 'Carvalho', 'Santos', '2001-02-15', 'Thorndon', 'Lambton Quay', 121, NULL, '99999', '6638907654', 'michael.brown@mailservice.com'),
-(13, 'Santiago', 'Lima', 'Ferreira', '2002-03-20', 'Fendalton', 'Riccarton Road', 1314, NULL, '22204', '6637011909', 'sarah.williams@webmail.com'),
-(14, 'Afonso', 'Moreira', 'Pereira', '2003-04-25', 'Mount Eden', 'Ponsonby Road', 4151, NULL, '22322', '6634207779', 'john.smith@domain.org'),
-(15, 'Matias', 'Fernandes', 'Costa', '2004-05-30', 'Kelburn', 'Willis Street', 5654, NULL, '99988', '6635018869', 'laura.martin@emailprovider.com'),
-(16, 'Maria', 'Nogueira', 'Oliveira', '2005-06-05', 'Riccarton', 'Victoria Street', 568, NULL, '22334', '6634449088', 'james.davis@company.com'),
-(17, 'Leonor', 'Teixeira', 'Martins', '2006-07-10', 'Remuera', 'Khyber Pass Road', 89, NULL, '22255', '6635558767', 'anna.lee@freemail.net'),
-(18, 'Matilde', 'Pinto', 'Sousa', '2007-08-15', 'Karori', 'Colombo Street', 65, NULL, '77789', '663537393', 'robert.jones@online.com'),
-(19, 'Beatriz', 'Rodrigues', 'Ribeiro', '2008-09-20', 'Merivale', 'Dominion Road', 444, NULL, '20912', '6631245678', 'jessica.taylor@inbox.com'),
-(20, 'Mariana', 'Araujo', 'Almeida', '2009-10-25', 'Newmarket', 'Cuba Street', 323, NULL, '30098', '6633054586', 'david.clark@service.net');
+(1, 'Jane', 'Smith', 'Doe', '1999-11-10', 'Centro', 'Calle Primera', 123, 12, '22100', '6641112223', 'jane.smith@hotmail.com'),
+(2, 'Mike', 'Brown', 'Jones', '1980-02-15', 'Chapultepec', 'Avenida Revolucion', 456, 19, '22200', '6642223334', 'mike.brown@gmail.com'),
+(3, 'Lucy', 'Williams', 'Liu', '1990-03-20', 'La Joya', 'Boulevard Agua Caliente', 789, 55, '22300', '6643334445', 'lucy.williams@yahoo.com'),
+(4, 'Mark', 'Martinez', 'Taylor', '2000-04-20', 'El Dorado', 'Callejon del Sol', 101, 30, '22400', '6644445556', 'mark.martinez@hotmail.com'),
+(5, 'Sophia', 'Davis', 'Wilson', '2001-05-05', 'Los Alamos', 'Avenida de la Paz', 202, NULL, '22500', '6645556667', 'sophia.davis@gmail.com'),
+(6, 'Liam', 'Garcia', 'Johnson', '2003-06-12', 'San Angel', 'Calle Lazaro Cardenas', 303, 22, '22600', '6646667778', 'liam.garcia@yahoo.com'),
+(7, 'Olivia', 'Rodriguez', 'Martinez', '2005-07-18', 'La Cumbre', 'Boulevard Diaz Ordaz', 404, NULL, '22700', '6647778889', 'olivia.rodriguez@hotmail.com'),
+(8, 'Emma', 'Hernandez', 'Brown', '1979-08-22', 'Bella Vista', 'Callejon de los Rios', 505, 77, '22800', '6648889990', 'emma.hernandez@gmail.com'),
+(9, 'Noah', 'Lopez', 'Clark', '1988-09-07', 'Las Palmas', 'Avenida Constitucion', 606, NULL, '22900', '6649990001', 'noah.lopez@yahoo.com'),
+(10, 'Ava', 'Gonzalez', 'Harris', '1982-10-09', 'Villa del Prado', 'Calle Benito Juarez', 707, 33, '23000', '6640001112', 'ava.gonzalez@hotmail.com'),
+(11, 'Joao', 'Gomes', 'Silva', '2000-01-10', 'Parnell', 'Queen Street', 701, 77, '19091', '6631246722', 'joao.gomes@gmail.com'),
+(12, 'Francisco', 'Carvalho', 'Santos', '2001-02-15', 'Thorndon', 'Lambton Quay', 121, NULL, '99999', '6638907654', 'francisco.carvalho@yahoo.com'),
+(13, 'Santiago', 'Lima', 'Ferreira', '2002-03-20', 'Fendalton', 'Riccarton Road', 1314, 55, '22204', '6637011909', 'santiago.lima@hotmail.com'),
+(14, 'Afonso', 'Moreira', 'Pereira', '2003-04-25', 'Mount Eden', 'Ponsonby Road', 4151, NULL, '22322', '6634207779', 'afonso.moreira@gmail.com'),
+(15, 'Matias', 'Fernandes', 'Costa', '2004-05-30', 'Kelburn', 'Willis Street', 5654, NULL, '99988', '6635018869', 'matias.fernandes@yahoo.com'),
+(16, 'Maria', 'Nogueira', 'Oliveira', '2005-06-05', 'Riccarton', 'Victoria Street', 568, NULL, '22334', '6634449088', 'maria.nogueira@hotmail.com'),
+(17, 'Leonor', 'Teixeira', 'Martins', '2006-07-10', 'Remuera', 'Khyber Pass Road', 89, 90, '22255', '6635558767', 'leonor.teixeira@gmail.com'),
+(18, 'Matilde', 'Pinto', 'Sousa', '2007-08-15', 'Karori', 'Colombo Street', 65, NULL, '77789', '663537393', 'matilde.pinto@yahoo.com'),
+(19, 'Beatriz', 'Rodrigues', 'Ribeiro', '2008-09-20', 'Merivale', 'Dominion Road', 444, NULL, '20912', '6631245678', 'beatriz.rodrigues@hotmail.com'),
+(20, 'Mariana', 'Araujo', 'Almeida', '2009-10-25', 'Newmarket', 'Cuba Street', 323, 99, '30098', '6633054586', 'mariana.araujo@gmail.com');
+
 
 
 INSERT INTO rol (idRol, nombre, descripcion) VALUES
@@ -31,16 +32,16 @@ INSERT INTO rol (idRol, nombre, descripcion) VALUES
 (4, 'Gerente', 'Encargado de la gestión de usuarios');
 
 INSERT INTO usuario (idUsuario, nombreUsuario, contrasena, rfc, rol, Persona) VALUES
-(1, 'Jane', 'utt1', 'GAAJ920101XXX', 1, 1),
-(2, 'Mike', 'utt2', 'RAAA890908XXX', 3, 2),
-(3, 'Lucy', 'utt3', 'TOMG860109XXX', 2, 3),
-(4, 'Mark', 'utt4', 'GULG930210XXX', 4, 4),
-(5, 'Sophia', 'utt5', 'ROJJ850303XXX', 3, 5),
-(6, 'Liam', 'utt6', 'MUGL950504XXX', 2, 6),
-(7, 'Olivia', 'utt7', 'LOAA910706XXX', 1, 7),
-(8, 'Emma', 'utt8', 'PEAF840807XXX', 4, 8),
-(9, 'Noah', 'utt9', 'PKJJ850303XXX', 2, 9),
-(10, 'Ava', 'utt10', 'FKD12E1ZXXX', 1, 10)
+(1, 'Jane', 'utt1', 'SIDJ991110XXX', 1, 1),
+(2, 'Mike', 'utt2', 'BOJM800215XXX', 3, 2),
+(3, 'Lucy', 'utt3', 'WILL900320XXX', 2, 3),
+(4, 'Mark', 'utt4', 'MATM000420XXX', 4, 4),
+(5, 'Sophia', 'utt5', 'DAWS010505XXX', 3, 5),
+(6, 'Liam', 'utt6', 'GAJL030612XXX', 2, 6),
+(7, 'Olivia', 'utt7', 'ROMO050718XXX', 1, 7),
+(8, 'Emma', 'utt8', 'HEBE790822XXX', 4, 8),
+(9, 'Noah', 'utt9', 'LOCL880907XXX', 2, 9),
+(10, 'Ava', 'utt10', 'GOHA821009XXX', 1, 10)
 
 INSERT INTO cliente VALUES
 (1, 11),
