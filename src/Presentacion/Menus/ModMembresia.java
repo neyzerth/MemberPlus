ba779aa445a70574c2265248c5424467f17f199b
Neyzer <neyzertoledo@gmail.com>
2024-08-07 13:53:51 -0700

    siu

--- src/Presentacion/Menus/ModMembresia.java
+++ src/Presentacion/Menus/ModMembresia.java
@@ -223,8 +223,6 @@ class SubmodTarjeta extends Menu {
                     tabla();
                     System.out.println();
                     Texto.esperarEnter(Color.verde(" " + modSing + " eliminado con exito"));
-                    Movimiento.registrarMovimiento( "Cambiar nivel", idTarjeta, 2);
-                    
                 
                     repetir = false;
                     return;
