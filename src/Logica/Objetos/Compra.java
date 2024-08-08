c8285345398035e0f23993bc1bd13dd0f85bf567
Axel <yerlan_2004@outlook.com>
2024-08-08 02:15:40 -0700

    Regresa saldoRestante

--- src/Logica/Objetos/Compra.java
+++ src/Logica/Objetos/Compra.java
@@ -137,7 +137,8 @@ public class Compra {
         return puntos > 0 || cashback > 0 || descuento > 0;
     }
 
-    public void usarBeneficios(boolean puntos, boolean saldo) {
+    public float usarBeneficios(boolean puntos, boolean saldo) {
+        float saldoRestante = 0;
         if (puntos) {
             subtotal -= (int) (tarjeta.getPuntosConvertidos());
             tarjeta.usarPuntos();
@@ -148,11 +149,14 @@ public class Compra {
                 subtotal -= saldoDisponible;
                 tarjeta.usarSaldo();
             } else {
-                tarjeta.setSaldo(saldoDisponible- subtotal);
+                saldoRestante = saldoDisponible - subtotal;
+                tarjeta.setSaldo(saldoRestante);
                 subtotal = 0;
-                
+
             }
         }
+        return saldoRestante;
+
     }
 
     // GETTERS AND SETTERS
